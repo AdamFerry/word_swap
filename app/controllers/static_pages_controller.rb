4fee485b9cb17b771c1a8eca2be1e503057b4b3f
Adam J. Ferry <adam.james.ferry@gmail.com>
2014-07-01 22:53:29 -0400

    Add Bootstrap. Add basic input form.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  
+  def swapped
+    @input_phrase = params[:input]
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 WordSwap::Application.routes.draw do
-  get "static_pages/home"
+   get "static_pages/home"
+   #root  'static_pages#home'
+   post '/static_pages/home' => 'static_pages#swapped'
+   
+   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
